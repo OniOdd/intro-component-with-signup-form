a05a5847fe10098cb5b3854e92e6ca12ec004a50
Mykyta Kavetskyi <103828218+OniOdd@users.noreply.github.com>
2024-02-24 16:02:09 +0200

    swapped import css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import './App.css';
 import Header from "./components/Header/Header.tsx";
 import Main from "./components/MainContent/MainContent.tsx";
-import './App.css';
 
 function App() {
   return (
